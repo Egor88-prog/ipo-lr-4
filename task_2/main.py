1d131ada9be261e8641f7f21020003d577d04298
Egor <gurskijegor88@gmail.com>
2025-10-26 10:22:20 +0300

    First commit

--- task_2/main.py
+++ task_2/main.py
@@ -1,3 +1,4 @@
+#Egor
 number=input("Введите список чисел ").split() #создание списка
 sqer_number=[pow(int(i),2) for i in number] #возведение чисел в квадрат
 print("Cписок чисел в квадрате ",sqer_number)#Вывод нового списка
--- task_4/main.py
+++ task_4/main.py
@@ -1,3 +1,4 @@
+#Egor
 string=input("Введите список строк ").split() #создание списка
 string_3x=[i*3 for i in string] #цикл увелечения строк
 print (string_3x) #вывод списка строк
